4a35b6f4070ab3ded9310a701734bdfdd1192c07
bsubedi26 <bsubedi26@gmail.com>
2018-02-09 15:05:24 -0500

    added toast.css

--- client/src/components/toast.js
+++ client/src/components/toast.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import 'styles/toast.css'
+import 'styles/components/toast.css'
 
 const style = {
   backgroundColor: '#84845b'
