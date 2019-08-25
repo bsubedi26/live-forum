cac047514115a8181f44a325705756bbfa4656de
bsubedi26 <bsubedi26@gmail.com>
2019-08-25 13:00:01 -0400

    wip

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -2,17 +2,18 @@ module.exports = function (app) {
   const knex = app.get('knexClient');
   const table = 'users';
 
+  // initial schema
   knex.schema.hasTable(table).then(exists => {
-
     if (!exists) {
-      knex.schema.createTable(table, t => {
-        t.increments('id').primary();
-        t.string('email').unique().notNullable();
-        t.string('password').notNullable();
-        t.string('avatar').notNullable();
+      knex.schema.createTable(table, table => {
+        table.increments('id').primary();
+        table.string('email').unique().notNullable();
+        table.string('password').notNullable();
+        table.string('avatar').notNullable();
 
-        t.integer('login_attempts').defaultTo(4);
-        t.timestamps(true, true);
+        table.integer('login_attempts').defaultTo(4);
+        table.boolean('active').defaultTo(true);
+        table.timestamps(true, true);
       })
         .then(() => console.log(`Created ${table} table`))
         .catch((e) => console.log(e));
--- server/src/services/database/index.js
+++ server/src/services/database/index.js
@@ -2,12 +2,10 @@ const user = require('./user');
 const thread = require('./thread');
 const comment = require('./comment');
 const topic = require('./topic');
-const blog = require('./blog');
 
 module.exports = function (app) {
   app.configure(user);
   app.configure(topic);
   app.configure(thread);
   app.configure(comment);
-  app.configure(blog);
 };
