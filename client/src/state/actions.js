8ba9f3c70783722f2e19fdae59713b5c7a60a3ad
bsubedi26 <bsubedi26@gmail.com>
2019-08-31 16:02:06 -0400

    rm unused file

--- client/src/state/actions.js
+++
@@ -1,9 +0,0 @@
-import * as Services from '../services'
-
-export const create = async (service, payload) => {
-  if (Services[service]) {
-    const data = await Services[service].create(payload)
-    return data
-  }
-  return null
-}
